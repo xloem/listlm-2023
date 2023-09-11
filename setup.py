e7b2b94bbe21b5276bf50f3a5d5a558a4e1669ef
John Doe <johndoe@example.com>
2023-09-11 10:23:13 -0700

    requirements->setup

--- setup.py
+++ setup.py
@@ -1,2 +1,11 @@
 from setuptools import setup
-setup()
+setup(
+    install_requires=[
+        'transformers>=4.33.0',
+        'optimum>=1.12.0',
+        'auto-gptq',
+    ],
+    dependency_links=[
+        'https://huggingface.github.io/autogptq-index/whl/cu118/',
+    ],
+)
